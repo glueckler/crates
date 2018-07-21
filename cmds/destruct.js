a3ec23f26b31ed0054b26f2880efc0cdfe746b4d
Dean Glueckler <glueckler@gmail.com>
2018-07-21 11:32:42 -0700

    added --destruct flag

---
+++ cmds/destruct.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-console */
+const fs = require('fs')
+const path = require('path')
+
+const destiny = '~~hislib'
+
+//
+//
+// helpers..
+const isDirectory = source => fs.lstatSync(source).isDirectory()
+
+// function to rm -rf
+const deleteFolderRecursive = function(path) {
+  if (path === '/') {
+    console.log('lets not delete our computer..')
+    return
+  }
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function(file, index) {
+      var curPath = path + '/' + file
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // recurse
+        deleteFolderRecursive(curPath)
+      } else {
+        // delete file
+        fs.unlinkSync(curPath)
+      }
+    })
+    fs.rmdirSync(path)
+  }
+}
+
+//
+//
+// destructure..
+module.exports = args => {
+  console.log(`\nstart destructuring crates into ${destiny}...\n...\n`)
+
+  // flags
+  const hi = args.hi
+
+  const dir = path.resolve('./')
+  const destinyPath = `${dir}/${destiny}`
+
+  const readDir = fs.readdirSync(dir)
+  const sourceDirs = readDir
+    .filter(isDirectory)
+    .filter(dirr => dirr[0] === '~' && dirr !== destiny)
+  deleteFolderRecursive(destinyPath)
+  fs.mkdirSync(destinyPath)
+
+  // copy each file with name beginning after >>
+  const copyFiles = dirr => {
+    if (dirr === '~hi' && !hi) return // don't copy ~hi folder
+    console.log(`\n\ndestructuring ${dirr}\n...\n\n`)
+    const files = fs.readdirSync(`${dir}/${dirr}`)
+    const destinySubPath = `${destinyPath}/${dirr.slice(1)}` // slice off the ~
+    fs.mkdirSync(destinySubPath)
+    files.forEach(file => {
+      const rename = file.slice(file.indexOf('>>') + 2).trim()
+      console.log(`touch.. ${rename}`)
+      fs.copyFileSync(`${dir}/${dirr}/${file}`, `${destinySubPath}/${rename}`)
+    })
+  }
+  if (sourceDirs.length === 0) {
+    console.log(`couldn't find any source dirs (starting with '~')`)
+  }
+  // apply copyFiles to all sourceDirs
+  sourceDirs.forEach(copyFiles)
+
+  console.log(`\n\nall done!\n`)
+}
--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+/* eslint-disable no-console */
 const minimist = require('minimist')
 
 module.exports = () => {
   const args = minimist(process.argv.slice(2))
-  const cmd = args._[0]
+  let cmd = args._[0]
 
+  if (args.h || args.help) {
+    cmd = 'help'
+  }
+  if (args.d || args.destruct) {
+    cmd = 'destruct'
+  }
   switch (cmd) {
-    // add additional commands here..
-    // case 'asdf':
-    //   require('./cmds/asdf')(args)
-    //   break
+    case 'help': {
+      console.log(`\n-d -> will destructure crates`)
+      console.log(
+        `  optional --hi flag will also destructure ~hi folder if it exists`
+      )
+      console.log(
+        `  note! that any songs you wish to destructure must be inside a '~' directory (or they will go unnoticed)`
+      )
+      console.log(`\n--hi -> will create a folder with all structured songs`)
+      break
+    }
+    case 'destruct':
+      require('./cmds/destruct')(args)
+      break
     default:
       require('./cmds/default')(args)
       break
